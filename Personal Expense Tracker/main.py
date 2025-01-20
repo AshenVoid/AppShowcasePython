6449819a555c13916773134630a25d0466d49ee6
AshenVoid <richardegydy@gmail.com>
2025-01-20 16:24:11 +0100

    Skeleton setup

--- Personal Expense Tracker/main.py
+++ Personal Expense Tracker/main.py
@@ -1,3 +1,18 @@
 from database_manager import DatabaseManager
 from expense_manager import ExpenseManager
 from visualizer import ExpenseVisualizer
+
+class ExpenseTrackerApp:
+    def __init__(self):
+        self.db_manager = DatabaseManager("expenses.db")
+        self.expense_manager = ExpenseManager(self.db_manager)
+        self.visualizer = ExpenseVisualizer()
+
+    def run(self):
+        #TODO - UI & tie together
+        pass
+
+
+if __name__ == "__main__":
+    app = ExpenseTrackerApp()
+    app.run()
--- Personal Expense Tracker/visualizer.py
+++ Personal Expense Tracker/visualizer.py
@@ -3,3 +3,11 @@ import matplotlib.pyplot as plt
 class ExpenseVisualizer:
     def __init__(self):
         pass
+
+    def plot_expenses_by_category(self, expenses):
+        #TODO - matplotlib to plot
+        pass
+
+    def plot_monthly_expenses(self, expenses):
+        #TODO - matplotlib to plot
+        pass
